d2d63da9dc8a24bf5c29e946da5cd61d0099e8e4
Jerome Wei <jeromew@berkeley.edu>
2018-05-18 16:42:20 -0700

    f

--- speech.py
+++ speech.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import time
+import subprocess
+from gtts import gTTS
 
 def getSpeech(recognizer, mike):
     with mike as source:
@@ -20,6 +22,11 @@ def getSpeech(recognizer, mike):
 
     return response
 
+def say(stuff):
+    tts = gTTS(stuff)
+    tts.save('temp.mp3')
+    subprocess.Popen(["mpg123", "-q", "temp.mp3"])
+
 def start():
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
@@ -29,7 +36,10 @@ def start():
         if speech["error"]:
             print(speech["error"])
         else:
-            print(speech["transcribe"])
+            words = speech["transcribe"]
+            print(words)
+            say("You said, " + words + ".")
+            
 
 if __name__ == "__main__":
     start()
